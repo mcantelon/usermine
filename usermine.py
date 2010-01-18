8f5d38faf0b81815c84c30d2223fdbf8249176f9
Mike Cantelon <mike@mike-cantelons-macbook.local>
2010-01-17 22:36:19 -0800

    Added GPL to usermine.

--- usermine.py
+++ usermine.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import sqlite3
 import urllib2
 import simplejson
@@ -8,9 +9,22 @@ from operator import itemgetter
 from optparse import OptionParser
 
 """
-To-do:
-  *document usage in readme
-  *add GPL license
+Copyright 2010 Mike Cantelon
+
+This file is part of Usermine.
+
+Usermine is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+Usermine is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU General Public License along
+with Usermine. If not, see http://www.gnu.org/licenses/.
 """
 
 def get_command_line_arguments():
